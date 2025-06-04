5068904e074b37d87686118f6e7e1575f873b0df
Jacko0b <jakub.sobota.pl@gmail.com>
2025-06-04 23:53:51 +0200

    pr fixes

--- KredaServer.Infrastructure/PgUsersRepository.cs
+++ KredaServer.Infrastructure/PgUsersRepository.cs
@@ -12,7 +12,7 @@ public class PgUsersRepository(NpgsqlConnection connection) : IUsersRepository
 
     public async Task<string[]> GetUsernames(CancellationToken cancellationToken)
     {
-        var result = await connection.QueryAsync<string>("SELECT username FROM users;");
+        var result = await connection.QueryAsync<string>("SELECT username FROM users;", cancellationToken);
         return [.. result];
     }
 
@@ -22,7 +22,7 @@ public class PgUsersRepository(NpgsqlConnection connection) : IUsersRepository
             @"INSERT INTO users(id, username, created_at) 
             VALUES(@Id, @Username, @CreatedAt)",
             user,
-            cancellationToken:cancellationToken
+            cancellationToken: cancellationToken
         ));
     }
 }
